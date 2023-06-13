b36044b3a820f5b865c2e3885ade1a106e4363f9
Omar <omerhussein2608@hotmail.com>
2023-06-13 09:48:50 +0200

    Initial commit

---
+++ src/main/java/com/example/digitsum/DigitsumApplication.java
@@ -0,0 +1,13 @@
+package com.example.digitsum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitsumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitsumApplication.class, args);
+    }
+
+}
